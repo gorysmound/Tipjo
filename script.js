47120f29ac08fad5999edcbdbcbceb0ff349c915
gorysmound <115690849+gorysmound@users.noreply.github.com>
2024-01-10 14:44:44 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function pesanBurjo(namaBurjo, hargaBurjo) {
+    alert("Terima kasih telah memesan " + namaBurjo + " seharga Rp " + hargaBurjo );
+    // Anda dapat menambahkan logika atau integrasi backend di sini
+}
